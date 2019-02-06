5f4836e2fe56ce87a39589d167aeae73b8ab9031
sikira <maid992@gmail.com>
2019-02-06 22:22:22 +0100

    Added TypeOrm

--- server/app.ts
+++ server/app.ts
@@ -1,19 +1,28 @@
 import * as express from 'express'
 import { api } from './api';
+import { createConnection } from 'typeorm';
+import { User } from './entity/User';
 
-const app = express()
+createConnection().then(async connection => {
+  
+  const app = express()
+  
+  app.use(express.static('dist', {
+    extensions: ['html', 'htm']
+  }))
+  
+  app.get('/', (req, res) => {
+    res.sendFile('../dist/index.html')
+  })
+  
+  api(app)
+  
+  app.listen(3000, () => {
+    console.log('Server started on port 3000!!!')
+  })
 
+  console.log("Loading users from the database...");
+  const users = await connection.manager.find(User);
+  console.log("Loaded users: ", users);
 
-app.use(express.static('dist', {
-  extensions: ['html', 'htm']
-}))
-
-app.get('/', (req, res) => {
-  res.sendFile('../dist/index.html')
-})
-
-api(app)
-
-app.listen(3000, () => {
-  console.log('Server started on port 3000!!!')
-})
+}).catch(error => console.log(error));
